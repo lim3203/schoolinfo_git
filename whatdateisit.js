f05ab7f7d4936b913e5e0b203573d95b061af0e3
siyoung5100 <53399722+siyoung5100@users.noreply.github.com>
2021-10-17 01:35:59 +0900

    cm

--- whatdateisit.js
+++ whatdateisit.js
@@ -1,17 +1,20 @@
 const dateabc = new Date(),
-  dateA = document.querySelector(".datea");
-  dateB = document.querySelector(".dateb");
-  dateC = document.querySelector(".datec");
+  dateA = document.querySelector(".datea"),
+  dateB = document.querySelector(".dateb"),
+  dateC = document.querySelector(".datec"),
+  leftButton = document.querySelector(".left"),
+  rightButton = document.querySelector(".right");
 
 function getDate(){
   console.log(dateabc);
-  dateA.innerHTML =`${date.getMonth()+1}월 ${date.getDate()}일`;
-  dateC.innerHTML =`${date.getMonth()+1}월 ${date.getDate()}일`;
+  dateA.innerHTML =`${date.getMonth()+1}월 ${date.getDate()}일`,
+  dateC.innerHTML =`${date.getMonth()+1}월 ${date.getDate()}일`,
   dateB.innerHTML =`${date.getMonth()+1}월 ${date.getDate()+1}일`;
 }
 
 function init(){
-  getDate();  
+  getDate();
+
 }
 
 init();
